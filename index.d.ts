15b86656538136bafc1429cc8f903ae325421e5c
Raj Deut <raj@webstar.net.au>
2024-05-29 12:41:28 +1000

    chore: package all files
    Wasn't including new src files in package.
    Updated type defs for Options

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,56 @@
 export type Options = {
 	/**
-	Change the output style.
+    Change the output style.
 
-	When `false`, returns the value in a [recompilable source form](https://ss64.com/osx/osascript.html).
+    When `false`, returns the value in a [recompilable source form](https://ss64.com/osx/osascript.html).
 
-	@default true
+    @default true
 
-	@example
-	```
-	import {runAppleScript} from 'run-applescript';
+    @example
+    ```
+    import {runAppleScript} from 'run-applescript';
 
-	const result = await runAppleScript('return "unicorn"', {humanReadableOutput: false});
+    const result = await runAppleScript('return "unicorn"', {humanReadableOutput: false});
 
-	console.log(result);
-	//=> '"unicorn"'
-	```
-	*/
+    console.log(result);
+    //=> '"unicorn"'
+    ```
+    */
 	readonly humanReadableOutput?: boolean;
+
+	/**
+    Additional arguments to pass to the script.
+
+    @default []
+
+    @example
+    ```
+    import {runAppleScript} from 'run-applescript';
+
+    const result = await runAppleScript('return "unicorn"', {args: ['arg1', 'arg2']});
+
+    console.log(result);
+    //=> '"unicorn"'
+    ```
+    */
+	readonly args?: string[];
+
+	/**
+    Whether to use 'spawn' or 'exec' for running the script.
+
+    @default 'exec'
+
+    @example
+    ```
+    import {runAppleScript} from 'run-applescript';
+
+    const result = await runAppleScript('return "unicorn"', {spawnOrExec: 'spawn'});
+
+    console.log(result);
+    //=> '"unicorn"'
+    ```
+    */
+	readonly spawnOrExec?: 'spawn' | 'exec';
 };
 
 /**
