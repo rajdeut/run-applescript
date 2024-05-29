62b3ce27d05778ac7df87c91d4da52b5ce2f0f9f
Raj Deut <raj@webstar.net.au>
2024-05-29 11:45:35 +1000

    feat: added spawn, multiline & args
    Allows the user to specify execution method using either spawn or execFile depending on output length and speed required.
    
    Introduced args option to pass arguments to script execution.
    
    Checks for multiline scripts and executes as required by splitting them into multiple args.
    
    Version bumped

--- index.js
+++ index.js
@@ -1,32 +1,131 @@
 import process from 'node:process';
 import {promisify} from 'node:util';
-import {execFile, execFileSync} from 'node:child_process';
+import {spawn, spawnSync, execFile, execFileSync} from 'node:child_process';
 
 const execFileAsync = promisify(execFile);
 
-export async function runAppleScript(script, {humanReadableOutput = true} = {}) {
-	if (process.platform !== 'darwin') {
-		throw new Error('macOS only');
-	}
-
-	const outputArguments = humanReadableOutput ? [] : ['-ss'];
-
-	const {stdout} = await execFileAsync('osascript', ['-e', script, outputArguments]);
-	return stdout.trim();
+export async function runAppleScript(
+	script,
+	{humanReadableOutput = true, spawnOrExec = 'exec', args = []} = {},
+) {
+	platformCheck();
+	return executeAppleScriptAsync({
+		script,
+		humanReadableOutput,
+		sync: false,
+		spawnOrExec,
+		args,
+	});
 }
 
-export function runAppleScriptSync(script, {humanReadableOutput = true} = {}) {
+export function runAppleScriptSync(
+	script,
+	{humanReadableOutput = true, spawnOrExec = 'exec', args = []} = {},
+) {
+	platformCheck();
+	return executeAppleScriptSync({
+		script,
+		humanReadableOutput,
+		sync: true,
+		spawnOrExec,
+		args,
+	});
+}
+
+function platformCheck() {
 	if (process.platform !== 'darwin') {
 		throw new Error('macOS only');
 	}
+}
 
+function prepareScript(script) {
+	if (!Array.isArray(script)) {
+		script = script.split('\n');
+	}
+
+	// Loop through script and add another element containing "-e" in front of each item
+	return script.map(item => '-e ' + item.replaceAll('\\', '\\\\'));
+	// .replaceAll('"', '\\"')]);
+}
+
+function executeAppleScriptSync({
+	script,
+	humanReadableOutput = true,
+	args = [],
+	spawnOrExec = 'spawn',
+}) {
 	const outputArguments = humanReadableOutput ? [] : ['-ss'];
 
-	const stdout = execFileSync('osascript', ['-e', script, ...outputArguments], {
-		encoding: 'utf8',
-		stdio: ['ignore', 'pipe', 'ignore'],
-		timeout: 500,
-	});
+	// ExecFile - Sync
+	if (spawnOrExec === 'exec') {
+		const stdout = execFileSync(
+			'osascript',
+			[...prepareScript(script), ...args, ...outputArguments],
+			{
+				encoding: 'utf8',
+				stdio: ['ignore', 'pipe', 'ignore'],
+				timeout: 500,
+			},
+		);
+		return stdout.trim();
+	}
 
-	return stdout.trim();
+	// Spawn - Sync
+	const result = spawnSync(
+		'osascript',
+		[...prepareScript(script), ...args, ...outputArguments],
+		{
+			stdio: ['ignore', 'pipe', 'ignore'],
+		},
+	);
+
+	if (result.status !== 0) {
+		throw new Error(
+			`AppleScript exited with code ${result.status}: ${result.stderr.toString()}`,
+		);
+	}
+
+	return result.stdout.toString().trim();
+}
+
+async function executeAppleScriptAsync({
+	script,
+	humanReadableOutput = true,
+	args = [],
+	spawnOrExec = 'spawn',
+}) {
+	const outputArguments = humanReadableOutput ? [] : ['-ss'];
+
+	// ExecFile - Async
+	if (spawnOrExec === 'exec') {
+		const {stdout} = await execFileAsync('osascript', [
+			...prepareScript(script),
+			...args,
+			...outputArguments,
+		]);
+		return stdout.trim();
+	}
+
+	// Spawn - ASync
+	const child = spawn('osascript', [
+		...prepareScript(script),
+		...args,
+		...outputArguments,
+	]);
+	return new Promise((resolve, reject) => {
+		let stdout = '';
+		child.stdout.on('data', data => {
+			stdout += data.toString();
+		});
+
+		child.on('error', error => reject(error));
+
+		child.on('close', code => {
+			if (code === 0) {
+				resolve(stdout.trim());
+			} else {
+				reject(new Error(`AppleScript exited with code ${code}`));
+			}
+		});
+	});
 }
--- test.js
+++ test.js
@@ -1,26 +1,58 @@
 import test from 'ava';
 import {runAppleScript, runAppleScriptSync} from './index.js';
 
-test('async', async t => {
+test('exec - async', async t => {
 	t.is(await runAppleScript('return "unicorn"'), 'unicorn');
 });
 
-test('sync', t => {
+test('exec - sync', t => {
 	t.is(runAppleScriptSync('return "unicorn"'), 'unicorn');
 });
 
-test('async - non-human readable output', async t => {
+test('exec - async - non-human readable output', async t => {
 	t.is(await runAppleScript('return "unicorn"', {humanReadableOutput: false}), '"unicorn"');
 });
 
-test('sync - non-human readable output', t => {
+test('exec - sync - non-human readable output', t => {
 	t.is(runAppleScriptSync('return "unicorn"', {humanReadableOutput: false}), '"unicorn"');
 });
 
-test('async - non-human readable output (arrays)', async t => {
+test('exec - async - non-human readable output (arrays)', async t => {
 	t.is(await runAppleScript('return {"unicorn"}', {humanReadableOutput: false}), '{"unicorn"}');
 });
 
-test('sync - non-human readable output (arrays)', t => {
+test('exec - sync - non-human readable output (arrays)', t => {
 	t.is(runAppleScriptSync('return {"unicorn"}', {humanReadableOutput: false}), '{"unicorn"}');
+});
+
+test('spawn - async', async t => {
+	t.is(await runAppleScript('return "unicorn"', {spawnOrExec: 'spawn'}), 'unicorn');
+});
+
+test('spawn - sync', t => {
+	t.is(runAppleScriptSync('return "unicorn"', {spawnOrExec: 'spawn'}), 'unicorn');
+});
+
+test('spawn - async - non-human readable output', async t => {
+	t.is(await runAppleScript('return "unicorn"', {humanReadableOutput: false, spawnOrExec: 'spawn'}), '"unicorn"');
+});
+
+test('spawn - sync - non-human readable output', t => {
+	t.is(runAppleScriptSync('return "unicorn"', {humanReadableOutput: false, spawnOrExec: 'spawn'}), '"unicorn"');
+});
+
+test('spawn - async - non-human readable output (arrays)', async t => {
+	t.is(await runAppleScript('return {"unicorn"}', {humanReadableOutput: false, spawnOrExec: 'spawn'}), '{"unicorn"}');
+});
+
+test('spawn - sync - non-human readable output (arrays)', t => {
+	t.is(runAppleScriptSync('return {"unicorn"}', {humanReadableOutput: false, spawnOrExec: 'spawn'}), '{"unicorn"}');
+});
+
+test('argument passing', async t => {
+	t.is(await runAppleScript(['on run argv', 'return item 1 of argv', 'end run'], {args: ['unicorn', 'cake']}), 'unicorn');
+});
+
+test('multiline script with args', async t => {
+	t.is(await runAppleScript('on run argv\nreturn item 1 of argv\nend run', {args: ['unicorn', 'cake']}), 'unicorn');
 });
